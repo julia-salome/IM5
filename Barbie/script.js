35287e46df5f700ecba7dc21e8702b2896a3d989
julia-salome <152802213+julia-salome@users.noreply.github.com>
2023-12-03 18:00:57 +0100

    Update script.js

--- Barbie/script.js
+++ Barbie/script.js
@@ -1,17 +1,16 @@
 
 
-// script.js
 
 $(document).ready(function () {
     $('form').submit(function (event) {
-        event.preventDefault(); // Verhindert das Standardverhalten des Formulars (Seite neu laden)
+        event.preventDefault(); 
 
         $.ajax({
             type: $(this).attr('method'),
             url: $(this).attr('action'),
             data: $(this).serialize(),
             success: function (response) {
-                // Hier kannst du die Bestätigungsnachricht einfügen
+
                 $('#newsletter-form').html('<p>Danke für deine Anmeldung!</p>');
             },
             error: function (error) {
